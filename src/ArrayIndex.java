056f75beed0c378ac9b340471a99e1cabc5e3fc8
msuleman91 <msuleman387@gmail.com>
2019-10-03 06:00:30 -0700

    firstcommit

---
+++ src/ArrayIndex.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ArrayIndex {
+	
+	
+	public int indexReturn(String input) {
+
+		String in = input;
+		List<String> names = Arrays.asList("ali", "paul", "rizwan", "herry", "hassan");
+		System.out.println(names);
+		for (int i = 0; i <= names.size()-1; i++) {
+
+			if (names.get(i)==in) {
+				return i;
+			}
+
+		}
+		return -1;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayIndex obj = new ArrayIndex();
+		int valueindex=obj.indexReturn("paul");
+
+		System.out.println(valueindex);
+
+	}
+}
